e38e96f2a901ad2431e02270c7c1b0b8db47db85
Luoyi Lin <linluoyi666@outlook.com>
2026-05-30 21:09:04 +0800

    feat：初始化后端

---
+++ voice_calendar-backend/src/main/java/com/qiniu/voice_calendar/VoiceCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qiniu.voice_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceCalendarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceCalendarBackendApplication.class, args);
+    }
+
+}
